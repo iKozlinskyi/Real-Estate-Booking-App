6c95dc1e81025ef6074df4acccb207a7a59220b5
iKozlinskyi <ikozlinskyi@gmail.com>
2020-01-09 22:36:39 +0200

    initial commit - initiallize app server side

---
+++ src/main/java/com/ikozlinskyi/realestatebookingapp/RealEstateBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ikozlinskyi.realestatebookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateBookingAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RealEstateBookingAppApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ikozlinskyi/realestatebookingapp/RealEstateBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikozlinskyi.realestatebookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateBookingAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
