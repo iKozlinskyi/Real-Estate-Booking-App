557f00baa8008e5ceed0c86f25a233530934d57d
iKozlinskyi <ikozlinskyi@gmail.com>
2020-01-20 20:49:35 +0200

    Add pagination
    - Create withPagination HOC

---
+++ frontend/src/components/HOCs/withPagination.js
@@ -0,0 +1,84 @@
+import React, {Component} from 'react';
+
+const withPagination = WrappedComponent => {
+  return class extends Component {
+
+    static defaultProps = {
+      itemsPerPage: 4
+    };
+
+    constructor() {
+      super();
+
+      this.state = {
+        currentPageNumber: 0,
+        nextPageAvailable: true,
+        previousPageAvailable: false
+      };
+
+      this.nextPage = this.nextPage.bind(this);
+      this.previousPage = this.previousPage.bind(this);
+    }
+
+    getTotalPageCount() {
+      const itemCount = this.props.data.length;
+      return Math.ceil(itemCount / this.props.itemsPerPage)
+    }
+
+    nextPage() {
+      if (this.state.currentPageNumber === this.getTotalPageCount() - 1) {
+        this.setState({nextPageAvailable: false});
+        return;
+      }
+
+      this.setState(curState => ({
+        currentPageNumber: curState.currentPageNumber + 1
+      }))
+    }
+
+    previousPage() {
+      if (this.state.currentPageNumber === 0) {
+        this.setState({previousPageAvailable: false});
+        return;
+      }
+
+      this.setState(curState => ({
+        currentPageNumber: curState.currentPageNumber - 1
+      }))
+    }
+
+    getCurrentPageData() {
+      const {data, itemsPerPage} = this.props;
+      const minIndex = itemsPerPage * this.state.currentPageNumber;
+      const maxIndex = minIndex + itemsPerPage;
+
+      const currentPageData = data.slice(minIndex, maxIndex);
+
+      return currentPageData;
+    }
+
+    render() {
+
+      const {
+        nextPageAvailable,
+        previousPageAvailable,
+        currentPageNumber
+      } = this.state;
+
+      return(
+          <WrappedComponent
+              {...this.props}
+              data={this.getCurrentPageData()}
+              pageCount={this.getTotalPageCount()}
+              nextPage={this.nextPage}
+              previousPage={this.previousPage}
+              nextPageAvailable={nextPageAvailable}
+              previousPageAvailable={previousPageAvailable}
+              currentPageNumber={currentPageNumber}
+          />
+      )
+    }
+  }
+};
+
+export {withPagination};
--- frontend/src/components/RealEstateList/RealEstateList.js
+++ frontend/src/components/RealEstateList/RealEstateList.js
@@ -2,19 +2,48 @@ import React, {Component} from 'react';
 import RealEstateCard from "../RealEstateCard/RealEstateCard";
 import {withElementClassName} from "../HOCs/withElementClassName";
 import "./RealEstateList.css"
+import {withPagination} from "../HOCs/withPagination";
 
 class RealEstateList extends Component {
   render() {
-    const {data, elementClassName} = this.props;
+    const {
+      data,
+      elementClassName,
+      pageCount,
+      currentPageNumber,
+      previousPage,
+      nextPage
+    } = this.props;
 
     const realEstateCards = data.map(item =>
         <RealEstateCard key={item.id} {...item} elementClassName="RealEstateList__RealEstateCard"/>);
     return (
-        <ul className={`RealEstateList ${elementClassName}`}>
-          {realEstateCards}
-        </ul>
+        <>
+          <ul className={`RealEstateList ${elementClassName}`}>
+            {realEstateCards}
+          </ul>
+          <div className="paging-control-block RealEstateList__paging-control-block">
+            <button
+                className="paging-button"
+                onClick={previousPage}
+            >
+              <i className="fas fa-chevron-left"></i>
+            </button>
+            <div className="page-status paging-control-block__page-status">
+              Page {currentPageNumber + 1} of {pageCount}
+            </div>
+            <button
+                className="paging-button"
+                onClick={nextPage}
+            >
+              <i className="fas fa-chevron-right"></i>
+            </button>
+          </div>
+        </>
     );
   }
 }
 
-export default withElementClassName(RealEstateList);
+export default withPagination(
+    withElementClassName(RealEstateList)
+);
