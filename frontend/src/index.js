c57e179c2008b439e1a3f1f1e49cd0c71f4f2f64
iKozlinskyi <ikozlinskyi@gmail.com>
2020-01-30 18:54:16 +0200

    Use BrowserRouter and proxy instead of HashRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App.js';
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>,
+    </BrowserRouter>,
     document.getElementById('root')
 );
